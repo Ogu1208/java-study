87e6a6f3cb2f5395a98dfb126ec3786a4269f496
Ogu1208 <kdasunb6@gmail.com>
2025-02-26 11:55:38 +0900

    feat: commit test

---
+++ src/main/java/com/ogu1208/sec01/literal/Application1.java
@@ -0,0 +1,8 @@
+package com.ogu1208.sec01.literal;
+
+public class Application1 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("안녕하세요");
+    }
+}
